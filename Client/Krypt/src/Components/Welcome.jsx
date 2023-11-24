51e37886963c4aaa4dbd81d9af116257c7f58ff7
Parvez Khan <137421089+Parvezkhan0@users.noreply.github.com>
2023-11-24 23:43:56 +0530

    Update Welcome.jsx

--- Client/Krypt/src/Components/Welcome.jsx
+++ Client/Krypt/src/Components/Welcome.jsx
@@ -6,7 +6,6 @@ import { Loader } from ".";
 import { shortenAddress } from "../utils/shortenAddress";
 import { TransactionContext } from "../context/TransactionContext";
 
-
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
